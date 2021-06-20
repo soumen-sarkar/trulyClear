fda5d7bd7e8c5b72b94da2fb8e3a6817735c417d
Mrinmaybiswas <mrinmaybiswas95@gmail.com>
2021-06-20 13:27:42 +0530

    btn effect and other fixing

--- js/global.js
+++ js/global.js
@@ -78,3 +78,34 @@ $("#vlModal").modal('show');
 
 
 
+(function(window, $) {
+
+  $(function() {
+
+    $('.bannerInfo a, .headerTop a , .welcomeInfo a, .productContent a,.allReview a,.bInfo > a,.showAllRevewBtn a,.cartCount  a,.rA  a').on('click', function(event) {
+      //event.preventDefault();
+      var $btn = $(this),
+        $div = $('<div/>'),
+        btnOffset = $btn.offset(),
+        xPos = event.pageX - btnOffset.left,
+        yPos = event.pageY - btnOffset.top;
+
+      $div.addClass('ripple-effect');
+      $div
+        .css({
+          height: $btn.height(),
+          width: $btn.height(),
+          top: yPos - ($div.height() / 2),
+          left: xPos - ($div.width() / 2),
+          background: $btn.data("ripple-color") || "#fff"
+        });
+      $btn.append($div);
+
+      window.setTimeout(function() {
+        $div.remove();
+      }, 2000);
+    });
+
+  });
+
+})(window, jQuery);
